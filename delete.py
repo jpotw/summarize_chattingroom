96533c1230fbdbc0d9c82d800442ffcad7254445
Junu Park <junupark1@cau.ac.kr>
2023-12-07 19:51:14 +0900

    first commit for v2

--- delete.py
+++ delete.py
@@ -1,43 +1,28 @@
 import datetime
-import os
+import streamlit as st
 
-# Calculate the target date as one week ago from the current date
-current_date = datetime.datetime.now()
-target_date = current_date - datetime.timedelta(days=7)
-
-desktop_dir = os.path.expanduser(r"C:\Users\parkj\OneDrive\바탕 화면")
-file_name = "revised_text_file.txt"  # Specify the desired file name
-file_path = os.path.join(desktop_dir, file_name)
+## txt파일 자르기 리턴값: 새로운 txt파일
 
 
-# Convert the target date to a string in the desired format
-target_date_str = target_date.strftime("%Y-%m-%d")  # Modify the format as needed
+def make_new_file(uploaded_document):
+    current_date = datetime.datetime.now()
+    target_date = current_date - datetime.timedelta(days=1)
+    file = uploaded_document.read().decode("utf-8", errors="ignore")
 
-# Specify the path to your text file
-source_file_path = r"C:\kakao_crawl\AI로 업무 자동화하기1.txt"
-
-# Read the content of the file
-with open(source_file_path, "rb") as file:
-    content = file.read().decode("utf-8", errors="ignore")
-
-found = False
-
-# Search for the target date in the content
-while not found and target_date >= current_date - datetime.timedelta(days=7):
-    target_date_str = target_date.strftime("%Y년 %m월 %d일")  # Modify the format as needed
-    if target_date_str in content:
-        found = True
-    else:
-        target_date -= datetime.timedelta(days=1)
-
-# If a matching date is found, remove content above that date
-if found:
-    start_index = content.index(target_date_str)
-    new_content = content[start_index:]
+    target_date_str = f"{target_date.year}년 {target_date.month}월 {target_date.day}일"
     
-    # Write the modified content back to the file in binary mode ('wb')
-    with open(file_path, "wb") as file:
-        file.write(new_content.encode("utf-8"))
-    print("Revised file saved to the desktop.")
-else:
-    print("No matching date found in the past week.")
+    if target_date_str in file:
+        st.write(f"{target_date_str}의 기록을 찾았습니다.")
+        start_index = file.index(target_date_str)
+        new_content = file[start_index:]
+        st.write("수정되었습니다.")
+        return new_content
+    else:
+        st.write(f"{target_date_str}의 기록을 찾을 수 없습니다.")
+        while target_date_str not in file:
+            target_date_str -= datetime.timedelta(days=1)
+        st.write(f"{target_date_str}의 기록을 찾았습니다.")
+        start_index = file.index(target_date_str)
+        new_content = file[start_index:]
+        st.write("수정되었습니다.")
+        return new_content
